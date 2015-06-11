67798b4cfa475b3d314d2ccd354ee4a4b03c2352
KyleAsaff <kyleasaff@gmail.com>
2015-06-10 22:48:30 -0300

    Added comments

--- scraper.rb
+++ scraper.rb
@@ -23,6 +23,7 @@ HANDLE_LIST.each do |handle|
   doc = Nokogiri.HTML(open(BASE_URL+handle))
   raw = doc.css('script')[5].text
   trim = raw.match(/window._sharedData = ((.*));/)[1]
+
   # Parse plain text to JSON
   data = JSON.parse(trim)
 
@@ -36,12 +37,13 @@ HANDLE_LIST.each do |handle|
   data = data["entry_data"]["ProfilePage"][0]["user"]
 
   KEY_PHRASES.each do |phrase|
-    # format data for non case-sensitive exact phrase matching (ignoring punctuation)
+    # Format data for non case-sensitive exact phrase matching (ignoring punctuation)
     phrase = phrase.downcase.gsub(/[^#a-z0-9\s]/i, '')
     phrase = phrase.center(phrase.length+2)
     stripped = data["biography"].downcase.gsub(/[^#a-z0-9\s]/i, '')
     stripped = stripped.center(stripped.length+2)
-    # check if bio includes key phrase
+
+    # Check if bio includes key phrase
     if stripped.include? phrase
       user = [{
                 'username' => data["username"],
@@ -49,7 +51,8 @@ HANDLE_LIST.each do |handle|
                 'followed_by' => data["followed_by"]["count"],
                 'follows' => data["follows"]["count"],
       }]
-      # save accounts with a positive match to CSV
+
+      # Save accounts with a positive match to CSV
       CSV.open("output.csv", "ab") do |csv|
         user.each do |x|
           csv << x.values
